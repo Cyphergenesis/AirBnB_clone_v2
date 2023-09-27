d468e6242eda1c8928217a653f21a37ccf90e20f
Cyphergenesis <josquilani200@gmail.com>
2023-09-27 20:27:26 +0100

    user

--- models/user.py
+++ models/user.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """Defines the User class."""
+
 from models.base_model import Base
 from models.base_model import BaseModel
 from sqlalchemy import Column
 from sqlalchemy import String
 from sqlalchemy.orm import relationship
+
 class User(BaseModel, Base):
 	"""Represents a user for a MySQL database.
 	Inherits from SQLAlchemy Base and links to the MySQL table
