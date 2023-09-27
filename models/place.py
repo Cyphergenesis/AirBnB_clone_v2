dba53e9a1c51ba1b4ee0bb96eff6c2c87a1fd671
Cyphergenesis <josquilani200@gmail.com>
2023-09-27 20:26:03 +0100

    place

--- models/place.py
+++ models/place.py
@@ -13,13 +13,14 @@ from sqlalchemy import Integer
 from sqlalchemy import String
 from sqlalchemy import Table
 from sqlalchemy.orm import relationship
+
 association_table = Table("place_amenity", Base.metadata,
-		Column("place_id", String(60),
-			ForeignKey("places.id"),
-			primary_key=True, nullable=False),
-		Column("amenity_id", String(60),
-			ForeignKey("amenities.id"),
-			primary_key=True, nullable=False))
+						Column("place_id", String(60),
+			   				ForeignKey("places.id"),
+							primary_key=True, nullable=False),
+						Column("amenity_id", String(60),
+			 				ForeignKey("amenities.id"),primary_key=True, nullable=False))
+
 class Place(BaseModel, Base):
 	"""Represents a Place for a MySQL database.
 	Inherits from SQLAlchemy Base and links to the MySQL table
@@ -47,8 +48,9 @@ class Place(BaseModel, Base):
 	"""
 	__tablename__ = "places"
 	city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
-	user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
-	name = Column(String(128), nullable=False)description = Column(String(1024))
+	user_id = Column(String(60), ForeignKey("users.id"),nullable=False)
+	name = Column(String(128), nullable=False)
+	description = Column(String(1024))
 	number_rooms = Column(Integer, default=0)
 	number_bathrooms = Column(Integer, default=0)
 	max_guest = Column(Integer, default=0)
@@ -56,8 +58,10 @@ class Place(BaseModel, Base):
 	latitude = Column(Float)
 	longitude = Column(Float)
 	reviews = relationship("Review", backref="place", cascade="delete")
-	amenities = relationship("Amenity", secondary="place_amenity", viewonly=False)
+	amenities = relationship("Amenity", secondary="place_amenity",
+						  viewonly=False)
 	amenity_ids = []
+	
 	if getenv("HBNB_TYPE_STORAGE", None) != "db":
 		@property
 		def reviews(self):
@@ -68,17 +72,16 @@ class Place(BaseModel, Base):
 					review_list.append(review)
 			return review_list
 
-			@property
-			def amenities(self):
-				"""Get/set linked Amenities."""
-				amenity_list = []
-
-				for amenity in list(models.storage.all(Amenity).values()):
-					if amenity.id in self.amenity_ids:
-						amenity_list.append(amenity)
+		@property
+		def amenities(self):
+			"""Get/set linked Amenities."""
+			amenity_list = []
+			for amenity in list(models.storage.all(Amenity).values()):
+				if amenity.id in self.amenity_ids:
+					amenity_list.append(amenity)
 				return amenity_list
-				@amenities.setter
-
-				def amenities(self, value):
-					if type(value) == Amenity:
-						self.amenity_ids.append(value.id)
+		
+		@amenities.setter
+		def amenities(self, value):
+			if type(value) == Amenity:
+				self.amenity_ids.append(value.id)
